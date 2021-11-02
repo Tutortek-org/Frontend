8df6dca16a49110e013b5e667771b46763e632ee
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-11-02 14:06:25 +0200

    Add items onto main activity

--- app/src/main/java/com/tutortekorg/tutortek/MainActivity.kt
+++ app/src/main/java/com/tutortekorg/tutortek/MainActivity.kt
@@ -3,10 +3,40 @@ package com.tutortekorg.tutortek
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tutortek.R
+import com.example.tutortek.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: OnboardingAdapter
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupOnboardingAdapter()
+        binding.viewPagerOnboarding.adapter = adapter
+    }
+
+    private fun setupOnboardingAdapter() {
+        val list = mutableListOf<OnboardingItem>()
+
+        list.add(OnboardingItem(
+            R.mipmap.ic_books,
+            "Learn",
+            "Learn new and exciting thins!")
+        )
+        list.add(OnboardingItem(
+            R.mipmap.ic_handshake,
+            "Meet",
+            "Meet your local Tutortek community!")
+        )
+        list.add(OnboardingItem(
+            R.mipmap.ic_money,
+            "Earn",
+            "Earn money while sharing knowledge!")
+        )
+
+        adapter = OnboardingAdapter(list)
     }
 }
