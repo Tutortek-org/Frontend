ec151fe2440d7f522d80285b1080a741aacf2d5d
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-11-03 12:47:32 +0200

    Add login UI logic

--- app/src/main/java/com/tutortekorg/tutortek/LoginActivity.kt
+++ app/src/main/java/com/tutortekorg/tutortek/LoginActivity.kt
@@ -1,11 +1,30 @@
 package com.tutortekorg.tutortek
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tutortekorg.tutortek.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener { onLoginClick() }
+        binding.imgNewAccount.setOnClickListener { onRegisterClick() }
+        binding.txtRegister.setOnClickListener { onRegisterClick() }
+    }
+
+    private fun onLoginClick() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay)
+    }
+
+    private fun onRegisterClick() {
+        startActivity(Intent(this, RegisterActivity::class.java))
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay)
     }
 }
