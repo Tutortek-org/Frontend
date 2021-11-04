9b3f3398f35965401a80f26a4bcabed079d92f72
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-11-04 14:37:59 +0200

    Add basic register logic

--- app/src/main/java/com/tutortekorg/tutortek/authentication/RegisterActivity.kt
+++ app/src/main/java/com/tutortekorg/tutortek/authentication/RegisterActivity.kt
@@ -3,9 +3,15 @@ package com.tutortekorg.tutortek.authentication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.tutortekorg.tutortek.HomeActivity
 import com.tutortekorg.tutortek.R
+import com.tutortekorg.tutortek.RequestSingleton
+import com.tutortekorg.tutortek.TutortekConstants
 import com.tutortekorg.tutortek.databinding.ActivityRegisterBinding
+import org.json.JSONObject
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
@@ -21,8 +27,46 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun onRegisterClick() {
+        val url = "${TutortekConstants.BASE_URL}/register"
+        val body = formRequestBody()
+        val request = JsonObjectRequest(Request.Method.POST, url, body,
+            {
+                login(body)
+            },
+            {
+                Toast.makeText(this, "Unexpected error while registering", Toast.LENGTH_SHORT).show()
+            }
+        )
+        RequestSingleton.getInstance(this).addToRequestQueue(request)
+    }
+
+    private fun formRequestBody(): JSONObject {
+        val email = binding.editTextEmailRegister.text.toString()
+        val password = binding.editTextPasswordRegister.text.toString()
+        val body = JSONObject()
+        body.put("email", email)
+        body.put("password", password)
+        body.put("role", 1) // TODO: Change this to a non-hardcoded role
+        return body
+    }
+
+    private fun login(body: JSONObject) {
+        val url = "${TutortekConstants.BASE_URL}/login"
+        body.remove("role")
+        val request = JsonObjectRequest(Request.Method.POST, url, body,
+            {
+                navigateTomHomeScreen()
+            },
+            {
+                Toast.makeText(this, "Unexpected error while logging in", Toast.LENGTH_SHORT).show()
+            }
+        )
+        RequestSingleton.getInstance(this).addToRequestQueue(request)
+    }
+
+    private fun navigateTomHomeScreen() {
         startActivity(Intent(this, HomeActivity::class.java))
-        overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right)
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay)
         finish()
     }
 
