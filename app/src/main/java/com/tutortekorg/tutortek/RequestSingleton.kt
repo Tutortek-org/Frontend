5608e8d9633f22c13556ddb63f92b048739d64e9
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-11-04 13:32:15 +0200

    Implement RequestSingleton

---
+++ app/src/main/java/com/tutortekorg/tutortek/RequestSingleton.kt
@@ -0,0 +1,31 @@
+package com.tutortekorg.tutortek
+
+import android.annotation.SuppressLint
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+class RequestSingleton(private val context: Context) {
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        @Volatile
+        private var INSTANCE: RequestSingleton? = null
+
+        fun getInstance(context: Context) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: RequestSingleton(context).also {
+                    INSTANCE = it
+                }
+            }
+    }
+
+    private val requestQueue: RequestQueue by lazy {
+        Volley.newRequestQueue(context.applicationContext)
+    }
+
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+}
