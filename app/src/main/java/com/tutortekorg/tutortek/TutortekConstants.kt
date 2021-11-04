3cb40392f3cbfc1c087d02d36c5d32d6912a5b37
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-11-04 14:08:55 +0200

    Implement base login logic

---
+++ app/src/main/java/com/tutortekorg/tutortek/TutortekConstants.kt
@@ -0,0 +1,8 @@
+package com.tutortekorg.tutortek
+
+class TutortekConstants {
+    companion object {
+        const val BASE_URL = "https://tutortek.herokuapp.com"
+        const val BASE_URL_LOCAL = "http://localhost:8080"
+    }
+}
--- app/src/main/java/com/tutortekorg/tutortek/authentication/LoginActivity.kt
+++ app/src/main/java/com/tutortekorg/tutortek/authentication/LoginActivity.kt
@@ -3,9 +3,15 @@ package com.tutortekorg.tutortek.authentication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.tutortekorg.tutortek.HomeActivity
 import com.tutortekorg.tutortek.R
+import com.tutortekorg.tutortek.RequestSingleton
+import com.tutortekorg.tutortek.TutortekConstants
 import com.tutortekorg.tutortek.databinding.ActivityLoginBinding
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -21,6 +27,29 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun onLoginClick() {
+        val url = "${TutortekConstants.BASE_URL}/login"
+        val body = formRequestBody()
+        val request = JsonObjectRequest(Request.Method.POST, url, body,
+            {
+                navigateTomHomeScreen()
+            },
+            {
+                Toast.makeText(this, "Incorrect email or password", Toast.LENGTH_SHORT).show()
+            }
+        )
+        RequestSingleton.getInstance(this).addToRequestQueue(request)
+    }
+
+    private fun formRequestBody(): JSONObject {
+        val email = binding.editTextEmail.text.toString()
+        val password = binding.editTextPassword.text.toString()
+        val body = JSONObject()
+        body.put("email", email)
+        body.put("password", password)
+        return body
+    }
+
+    private fun navigateTomHomeScreen() {
         startActivity(Intent(this, HomeActivity::class.java))
         overridePendingTransition(R.anim.slide_in_right, R.anim.stay)
         finish()
