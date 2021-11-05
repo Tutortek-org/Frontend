fd1ee8c86c343550e135c364658395e880d77f43
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-11-05 21:23:00 +0200

    Implement autologin

--- app/src/main/java/com/tutortekorg/tutortek/TutortekUtils.kt
+++ app/src/main/java/com/tutortekorg/tutortek/TutortekUtils.kt
@@ -1,7 +1,16 @@
 package com.tutortekorg.tutortek
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.tutortekorg.tutortek.authentication.LoginActivity
 import com.tutortekorg.tutortek.constants.TutortekConstants
+import com.tutortekorg.tutortek.onboarding.MainActivity
 import org.json.JSONObject
 
 class TutortekUtils {
@@ -19,6 +28,42 @@ class TutortekUtils {
             val editor = preferences.edit()
             editor.remove(TutortekConstants.TOKEN_KEY)
             editor.apply()
+        }
+
+        fun getJwtToken(context: Context) =
+            context.getSharedPreferences(TutortekConstants.AUTH_PREFERENCES, Context.MODE_PRIVATE)
+                .getString(TutortekConstants.TOKEN_KEY, "")
+
+        fun sendRefreshRequest(token: String, activity: Activity, shouldNavigateToHome: Boolean) {
+            val url = "${TutortekConstants.BASE_URL}/refresh"
+            val request = object : JsonObjectRequest(
+                Method.GET, url, null,
+                {
+                    navigateToHomeScreen(activity)
+                },
+                {
+                    navigateToLoginScreen(activity)
+                }
+            ){
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["Content-Type"] = "application/json"
+                    headers["Authorization"] = "Bearer $token"
+                    return headers
+                }
+            }
+            RequestSingleton.getInstance(activity).addToRequestQueue(request)
+        }
+
+        private fun navigateToLoginScreen(activity: Activity) {
+            activity.startActivity(Intent(activity, LoginActivity::class.java))
+            activity.finish()
+        }
+
+        private fun navigateToHomeScreen(activity: Activity) {
+            activity.startActivity(Intent(activity, HomeActivity::class.java))
+            activity.finish()
         }
     }
 }
