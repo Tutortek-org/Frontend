7a28d484959af242d3cbec1cda4b25867c9ed3c7
Zablas <vagabaliukas@gmail.com>
2021-11-17 19:29:49 +0200

    Fix date picker dialog resetting to current date

--- app/src/main/java/com/tutortekorg/tutortek/DatePickerFragment.kt
+++ app/src/main/java/com/tutortekorg/tutortek/DatePickerFragment.kt
@@ -6,23 +6,39 @@ import android.os.Bundle
 import android.widget.DatePicker
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import java.text.SimpleDateFormat
 import java.util.*
 
 class DatePickerFragment(private val viewToSet: EditText) : DialogFragment(),
     DatePickerDialog.OnDateSetListener {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        return DatePickerDialog(requireContext(), R.style.customDatePickerStyle, this, year, month, day)
+        if(viewToSet.text.isNullOrBlank()) return setDateToCurrentDay()
+        return setDateToCurrentInput()
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
         val dayToSet = if(day < 10) "0$day" else day.toString()
         val monthToSet = if(month < 9) "0${month + 1}" else (month + 1).toString()
         viewToSet.setText(getString(R.string.birthdate_value, year, monthToSet, dayToSet))
+    }
+
+    private fun setDateToCurrentDay(): DatePickerDialog {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(requireContext(), R.style.customDatePickerStyle, this, year, month, day)
+    }
+
+    private fun setDateToCurrentInput(): DatePickerDialog {
+        val parser = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val parsedDate = parser.parse(viewToSet.text.toString())
+        val calendar = Calendar.getInstance()
+        calendar.time = parsedDate!!
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(requireContext(), R.style.customDatePickerStyle, this, year, month, day)
     }
 }
