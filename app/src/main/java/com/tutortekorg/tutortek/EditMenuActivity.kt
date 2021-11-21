66401f924fe618a51536bf5f294c8c07d5c4f642
Zablas <vagabaliukas@gmail.com>
2021-11-21 12:50:23 +0200

    Add initial UI of the new edit menu

---
+++ app/src/main/java/com/tutortekorg/tutortek/EditMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.tutortekorg.tutortek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_menu)
+    }
+}
