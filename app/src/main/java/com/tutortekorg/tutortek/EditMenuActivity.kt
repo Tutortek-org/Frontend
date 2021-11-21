7a4eccb89273551bd6ad7a03d5df2398542d45e3
Zablas <vagabaliukas@gmail.com>
2021-11-21 13:04:42 +0200

    Change navigation order

--- app/src/main/java/com/tutortekorg/tutortek/EditMenuActivity.kt
+++ app/src/main/java/com/tutortekorg/tutortek/EditMenuActivity.kt
@@ -1,11 +1,23 @@
 package com.tutortekorg.tutortek
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tutortekorg.tutortek.databinding.ActivityEditMenuBinding
 
 class EditMenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_menu)
+        binding = ActivityEditMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnChangeProfileData.setOnClickListener { onEditProfileClick() }
+    }
+
+    private fun onEditProfileClick() {
+        val intent = Intent(this, ProfileEditActivity::class.java)
+        startActivity(intent)
     }
 }
