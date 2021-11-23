77f187d3d3e80143926b013e964270596b77e604
Zablas <vagabaliukas@gmail.com>
2021-11-23 22:31:56 +0200

    Add navigation logic to full topic list

--- app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/HomeFragment.kt
+++ app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/HomeFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.tutortekorg.tutortek.R
+import com.tutortekorg.tutortek.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.btnAllTopics.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_topicListFragment)
+        }
+        return binding.root
     }
 }
