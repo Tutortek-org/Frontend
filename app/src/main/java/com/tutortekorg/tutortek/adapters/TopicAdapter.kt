b2e34eb9fb970db61d7fa3a7507c7d65c540a3f2
Zablas <vagabaliukas@gmail.com>
2021-11-25 16:30:16 +0200

    Implement topic adapter

--- app/src/main/java/com/tutortekorg/tutortek/adapters/TopicAdapter.kt
+++ app/src/main/java/com/tutortekorg/tutortek/adapters/TopicAdapter.kt
@@ -1,4 +1,33 @@
 package com.tutortekorg.tutortek.adapters
 
-class TopicAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.tutortekorg.tutortek.R
+import com.tutortekorg.tutortek.data.Topic
+
+class TopicAdapter(private val topics: List<Topic>)
+    : RecyclerView.Adapter<TopicAdapter.TopicViewHolder>() {
+
+    class TopicViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private var textTitle: TextView = view.findViewById(R.id.txt_topic_name)
+
+        fun setTopicData(topic: Topic) {
+            textTitle.text = topic.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopicViewHolder =
+        TopicViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_container_topic, parent, false)
+        )
+
+    override fun onBindViewHolder(holder: TopicViewHolder, position: Int) {
+        holder.setTopicData(topics[position])
+    }
+
+    override fun getItemCount(): Int = topics.size
 }
---
+++ app/src/main/java/com/tutortekorg/tutortek/data/Topic.kt
@@ -0,0 +1,5 @@
+package com.tutortekorg.tutortek.data
+
+data class Topic(
+    val name: String
+)
