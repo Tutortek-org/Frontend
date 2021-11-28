8f997d73cbc8fc4ab8b42fee961336d42dcb6432
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-11-28 22:03:23 +0200

    Implement topic delete confirmation

--- app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/TopicDetailsFragment.kt
+++ app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/TopicDetailsFragment.kt
@@ -1,10 +1,12 @@
 package com.tutortekorg.tutortek.navigation_fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import com.tutortekorg.tutortek.R
 import com.tutortekorg.tutortek.data.Topic
 import com.tutortekorg.tutortek.databinding.FragmentTopicDetailsBinding
@@ -18,6 +20,12 @@ class TopicDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentTopicDetailsBinding.inflate(inflater, container, false)
+
+        binding.btnDeleteTopic.setOnClickListener { showConfirmDialog() }
+        binding.btnEditTopic.setOnClickListener {
+
+        }
+
         return binding.root
     }
 
@@ -25,5 +33,25 @@ class TopicDetailsFragment : Fragment() {
         super.onStart()
         topic = arguments?.getSerializable("topic") as Topic
         binding.txtTopicDetailsName.text = topic.name
+    }
+
+    private fun showConfirmDialog() {
+        val message = getString(R.string.topic_delete_question, topic.name)
+        val alert = AlertDialog.Builder(requireContext())
+            .setTitle(R.string.confirm_delete)
+            .setMessage(message)
+            .setPositiveButton(R.string.btn_no) { _: DialogInterface, _: Int -> }
+            .setNegativeButton(R.string.btn_yes) { _: DialogInterface, _: Int ->
+                sendTopicDeleteRequest()
+            }
+            .create()
+        val color = requireContext().getColor(R.color.color_primary)
+        alert.show()
+        alert.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(color)
+        alert.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(color)
+    }
+
+    private fun sendTopicDeleteRequest() {
+
     }
 }
