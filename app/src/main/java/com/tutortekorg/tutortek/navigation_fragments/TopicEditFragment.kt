baaadaf4e3fab4ee7c039cc9d3ee1b3f51276029
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-11-29 18:18:54 +0200

    Fix shadow for topic details card view

--- app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/TopicEditFragment.kt
+++ app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/TopicEditFragment.kt
@@ -6,13 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tutortekorg.tutortek.R
+import com.tutortekorg.tutortek.data.Topic
+import com.tutortekorg.tutortek.databinding.FragmentTopicEditBinding
 
 class TopicEditFragment : Fragment() {
+    private lateinit var binding: FragmentTopicEditBinding
+    private lateinit var topic: Topic
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_topic_edit, container, false)
+    ): View {
+        binding = FragmentTopicEditBinding.inflate(inflater, container, false)
+        binding.btnConfirmEditTopic.setOnClickListener { saveEditedTopic() }
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        topic = arguments?.getSerializable("topic") as Topic
+        binding.editTextTopicNameEdit.setText(topic.name)
+    }
+
+    private fun saveEditedTopic() {
+
     }
 }
