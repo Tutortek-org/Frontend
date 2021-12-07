f89182ddf7133c241b04dbce1aefa3d73121a640
Zablas <vagabaliukas@gmail.com>
2021-12-07 15:13:31 +0200

    Add layouts and their data classes

---
+++ app/src/main/java/com/tutortekorg/tutortek/adapters/LearningMaterialAdapter.kt
@@ -0,0 +1,4 @@
+package com.tutortekorg.tutortek.adapters
+
+class LearningMaterialAdapter {
+}
---
+++ app/src/main/java/com/tutortekorg/tutortek/data/LearningMaterial.kt
@@ -0,0 +1,8 @@
+package com.tutortekorg.tutortek.data
+
+data class LearningMaterial(
+    val id: Long,
+    var name: String,
+    var description: String,
+    var link: String
+)
