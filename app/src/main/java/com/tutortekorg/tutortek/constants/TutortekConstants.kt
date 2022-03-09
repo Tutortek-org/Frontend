190e5eed37a57df3e649e8aae76515adae81a9d7
Zablas <vagabaliukas@gmail.com>
2022-03-09 15:00:04 +0200

    Add setting saving

--- app/src/main/java/com/tutortekorg/tutortek/constants/TutortekConstants.kt
+++ app/src/main/java/com/tutortekorg/tutortek/constants/TutortekConstants.kt
@@ -3,9 +3,10 @@ package com.tutortekorg.tutortek.constants
 class TutortekConstants {
     companion object {
         const val BASE_URL = "https://tutortek.herokuapp.com"
-        const val BASE_URL_LOCAL = "http://localhost:8080"
         const val AUTH_PREFERENCES = "authPrefs"
         const val TOKEN_KEY = "token"
         const val BUNDLE_PROFILE = "profile"
+        const val DARK_MODE_FLAG = "isDarkModeOn"
+        const val SETTING_PREFERENCES = "settingPrefs"
     }
 }
--- app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/SettingsFragment.kt
+++ app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/SettingsFragment.kt
@@ -1,7 +1,9 @@
 package com.tutortekorg.tutortek.navigation_fragments
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,7 @@ import android.view.ViewGroup
 import com.tutortekorg.tutortek.R
 import com.tutortekorg.tutortek.utils.JwtUtils
 import com.tutortekorg.tutortek.authentication.LoginActivity
+import com.tutortekorg.tutortek.constants.TutortekConstants
 import com.tutortekorg.tutortek.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -20,7 +23,22 @@ class SettingsFragment : Fragment() {
     ): View {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
         binding.btnLogout.setOnClickListener { logout() }
+        binding.darkTheme.setOnClickListener { changeTheme() }
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val preferences = requireContext().getSharedPreferences(TutortekConstants.SETTING_PREFERENCES, Context.MODE_PRIVATE)
+        val isDarkModeOn = preferences.getBoolean(TutortekConstants.DARK_MODE_FLAG, false)
+        binding.darkTheme.isChecked = isDarkModeOn
+    }
+
+    private fun changeTheme() {
+        val preferences = requireContext().getSharedPreferences(TutortekConstants.SETTING_PREFERENCES, Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putBoolean(TutortekConstants.DARK_MODE_FLAG, binding.darkTheme.isChecked)
+        editor.apply()
     }
 
     private fun logout() {
