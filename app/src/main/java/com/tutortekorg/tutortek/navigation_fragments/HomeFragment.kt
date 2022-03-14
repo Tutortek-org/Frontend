8694ee0780d46b74e62a713967c290288584fd43
Zablas <vagabaliukas@gmail.com>
2022-03-15 00:12:12 +0200

    Update HomeFragment.kt

--- app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/HomeFragment.kt
+++ app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.tutortekorg.tutortek.navigation_fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
