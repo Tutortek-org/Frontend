dd5b080f6e7bf4c4ee15b9f92afe6874276c2d80
Zablas <vagabaliukas@gmail.com>
2022-03-24 16:48:26 +0200

    Move photo downloading to a utilities class

--- app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/HomeFragment.kt
+++ app/src/main/java/com/tutortekorg/tutortek/navigation_fragments/HomeFragment.kt
@@ -1,12 +1,10 @@
 package com.tutortekorg.tutortek.navigation_fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.findNavController
 import com.android.volley.Request
 import com.google.firebase.messaging.FirebaseMessaging
@@ -14,18 +12,10 @@ import com.tutortekorg.tutortek.R
 import com.tutortekorg.tutortek.constants.TutortekConstants
 import com.tutortekorg.tutortek.databinding.FragmentHomeBinding
 import com.tutortekorg.tutortek.requests.TutortekObjectRequest
-import com.tutortekorg.tutortek.requests.retrofit.FileDownloadService
-import com.tutortekorg.tutortek.requests.retrofit.ServiceGenerator
-import com.tutortekorg.tutortek.singletons.ProfileSingleton
 import com.tutortekorg.tutortek.singletons.RequestSingleton
 import com.tutortekorg.tutortek.utils.JwtUtils
-import okhttp3.ResponseBody
+import com.tutortekorg.tutortek.utils.SystemUtils
 import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.io.*
-import java.lang.Exception
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -49,7 +39,10 @@ class HomeFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
-        downloadProfilePhoto()
+        val profileId = JwtUtils.getProfileIdFromSavedToken(requireContext())
+        context?.let {
+            if (profileId != null) SystemUtils.downloadProfilePhoto(it, profileId)
+        }
         registerDeviceToken()
     }
 
@@ -62,58 +55,6 @@ class HomeFragment : Fragment() {
                 val request = TutortekObjectRequest(requireContext(), Request.Method.POST, url, body, {}, {})
                 RequestSingleton.getInstance(requireContext()).addToRequestQueue(request)
             }
-        }
-    }
-
-    private fun downloadProfilePhoto() {
-        val service = ServiceGenerator.createService(FileDownloadService::class.java)
-        val token = JwtUtils.getJwtToken(requireContext())
-        val call = service.downloadProfilePicture("Bearer $token")
-        call.enqueue(object : Callback<ResponseBody> {
-            override fun onResponse(call: Call<ResponseBody>?, response: Response<ResponseBody>?) {
-                if (response?.isSuccessful!!) savePhotoToDevice(response.body())
-            }
-
-            override fun onFailure(call: Call<ResponseBody>?, t: Throwable?) {}
-        })
-    }
-
-    private fun savePhotoToDevice(body: ResponseBody) {
-        try {
-            val filePath = context?.getExternalFilesDir(null).toString() + File.separator + "pfp"
-            val file = File(filePath)
-            var inputStream: InputStream? = null
-            var outputStream: OutputStream? = null
-            try {
-                val fileReader = ByteArray(4096)
-                inputStream = body.byteStream()
-                outputStream = FileOutputStream(file)
-
-                while (true) {
-                    val readResult = inputStream.read(fileReader)
-                    if(readResult == -1) break
-                    outputStream.write(fileReader, 0, readResult)
-                }
-
-                outputStream.flush()
-                ProfileSingleton.getInstance().userProfile?.photoPath = filePath
-            }
-            catch (e: IOException) {
-                try {
-                    Toast.makeText(requireContext(), R.string.error_photo_file_create, Toast.LENGTH_SHORT).show()
-                }
-                catch (e: Exception){}
-            }
-            finally {
-                inputStream?.close()
-                outputStream?.close()
-            }
-        }
-        catch (e: IOException) {
-            try {
-                Toast.makeText(requireContext(), R.string.error_photo_file_create, Toast.LENGTH_SHORT).show()
-            }
-            catch (e: Exception){}
         }
     }
 }
--- app/src/main/java/com/tutortekorg/tutortek/utils/SystemUtils.kt
+++ app/src/main/java/com/tutortekorg/tutortek/utils/SystemUtils.kt
@@ -2,8 +2,18 @@ package com.tutortekorg.tutortek.utils
 
 import android.content.Context
 import android.content.DialogInterface
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.tutortekorg.tutortek.R
+import com.tutortekorg.tutortek.requests.retrofit.FileDownloadService
+import com.tutortekorg.tutortek.requests.retrofit.ServiceGenerator
+import com.tutortekorg.tutortek.singletons.ProfileSingleton
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.*
+import java.lang.Exception
 
 object SystemUtils {
     fun showConfirmDeleteDialog(
@@ -25,5 +35,56 @@ object SystemUtils {
         alert.show()
         alert.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(color)
         alert.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(color)
+    }
+
+    fun downloadProfilePhoto(context: Context, id: Long) {
+        val service = ServiceGenerator.createService(FileDownloadService::class.java)
+        val token = JwtUtils.getJwtToken(context)
+        val call = service.downloadProfilePicture("Bearer $token", id)
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>?, response: Response<ResponseBody>?) {
+                if (response?.isSuccessful!!) savePhotoToDevice(context, response.body(), id)
+            }
+            override fun onFailure(call: Call<ResponseBody>?, t: Throwable?) {}
+        })
+    }
+
+    private fun savePhotoToDevice(context: Context, body: ResponseBody, id: Long) {
+        try {
+            val filePath = context.getExternalFilesDir(null).toString() + File.separator + "pfp$id.png"
+            val file = File(filePath)
+            var inputStream: InputStream? = null
+            var outputStream: OutputStream? = null
+            try {
+                val fileReader = ByteArray(4096)
+                inputStream = body.byteStream()
+                outputStream = FileOutputStream(file)
+
+                while (true) {
+                    val readResult = inputStream.read(fileReader)
+                    if(readResult == -1) break
+                    outputStream.write(fileReader, 0, readResult)
+                }
+
+                outputStream.flush()
+                ProfileSingleton.getInstance().userProfile?.photoPath = filePath
+            }
+            catch (e: IOException) {
+                try {
+                    Toast.makeText(context, R.string.error_photo_file_create, Toast.LENGTH_SHORT).show()
+                }
+                catch (e: Exception){}
+            }
+            finally {
+                inputStream?.close()
+                outputStream?.close()
+            }
+        }
+        catch (e: IOException) {
+            try {
+                Toast.makeText(context, R.string.error_photo_file_create, Toast.LENGTH_SHORT).show()
+            }
+            catch (e: Exception){}
+        }
     }
 }
