ebb6b53ccbce1f618d4201b309bb9d50f7df1026
Zablas <vagabaliukas@gmail.com>
2022-03-26 16:43:45 +0200

    Add notification toggle logic

--- app/src/main/java/com/tutortekorg/tutortek/constants/TutortekConstants.kt
+++ app/src/main/java/com/tutortekorg/tutortek/constants/TutortekConstants.kt
@@ -5,6 +5,7 @@ object TutortekConstants {
     const val AUTH_PREFERENCES = "authPrefs"
     const val TOKEN_KEY = "token"
     const val DARK_MODE_FLAG = "isDarkModeOn"
+    const val NOTIFICATIONS_FLAG = "areNotificationsAllowed"
     const val SETTING_PREFERENCES = "settingPrefs"
     const val NOTIFICATION_CHANNEL_ID = "tutortek_channel"
 }
